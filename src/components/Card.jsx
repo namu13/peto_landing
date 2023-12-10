5f2a048a02b7da801afc1b798b42dac076c2045b
namu <namun1303@gmail.com>
2023-12-11 00:53:02 +0100

    fix card.jsx title mragin

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -14,16 +14,16 @@ const Card = ({
         <span>{teamCompany}</span>
       </div>
       {image ? (
-        <div className="mt-4 bg-gradient-to-b from-zinc-900  to-[#0B0A09] to-95% aspect-square">
+        <div className="mt-4 mb-4 bg-gradient-to-b from-zinc-900  to-[#0B0A09] to-95% aspect-square">
           <img src={image} className="object-cover aspect-square" />
         </div>
       ) : (
-        <div className="mt-4 bg-gradient-to-b from-peto  to-[#0B0A09] to-95% aspect-square"></div>
+        <div className="mt-4 mb-4 bg-gradient-to-b from-peto  to-[#0B0A09] to-95% aspect-square"></div>
       )}
 
       <span className="text-3xl font-bold">{projectName}</span>
       <div className="h-14">
-        <p className="mt-2 text-zinc-400 line-clamp-2">{shortDescription}</p>
+        <p className="mt-1 text-zinc-400 line-clamp-2">{shortDescription}</p>
       </div>
       <span className="text-peto font-bold">
         # {topic === "Other" ? other : topic}
